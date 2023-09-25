c93222cf11d7fb649c13e859a0a76e5fe6f3f39e
yusuftekin <yusuftekin@tombll.com>
2023-09-25 10:01:13 +0300

    pickerinput alignment fixed

--- FormInput Example/FormInput Example/ViewController.swift
+++ FormInput Example/FormInput Example/ViewController.swift
@@ -10,6 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     var formView: FormView? { view as? FormView }
+    
+    let items = [
+        PickerItem(title: "Test1", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Test2", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Test3", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Test4", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Test5", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Test6", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Test7", subtitle: "Test", item: "Test"),
+    ]
+
+    let searchItems = [
+        PickerItem(title: "Test1", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Test2", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Test3", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Search1", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Search2", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Search3", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Search4", subtitle: "Test", item: "Test"),
+    ]
+
+    let selectedItems = [
+        PickerItem(title: "Selected1", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Selected2", subtitle: "Test", item: "Test"),
+        PickerItem(title: "Selected3", subtitle: "Test", item: "Test"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         formView?.parentViewController = self
@@ -17,7 +44,10 @@ class ViewController: UIViewController {
         let smallTextInput = FormInputView.create(name: "header", type: .SmallText)
         let tagTextInput = FormInputView.create(name: "tags", type: .TagText)
         let switchInput = FormInputView.create(name: "switch", value: true, type: .Switch)
-        let pickerInput = FormInputView.create(name: "picker", type: .Picker) as? FormPickerView
+        let pickerInput = createPickerInput()
+        let pickerInput2 = createPickerInput()
+        let pickerInput3 = createPickerInput()
+        let pickerInput4 = createPickerInput()
         let photoPickerInput = FormInputView.create(name: "photos", type: .PhotoPicker)
         let largeTextInput = FormInputView.create(name: "largeText", type: .LargeText)
         let datePickerInput = FormInputView.create(name: "largeText", type: .DatePicker)
@@ -31,55 +61,7 @@ class ViewController: UIViewController {
         tagTextInput?.title = "Tagler"
         largeTextInput?.title = "Açıklama"
         switchInput?.title = "Buton aç kapa"
-        pickerInput?.title = "Seçmek istediğini seç"
         photoPickerInput?.title = "Fotoğraf seçin"
-
-        let items = [
-            PickerItem(title: "Test1", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Test2", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Test3", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Test4", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Test5", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Test6", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Test7", subtitle: "Test", item: "Test"),
-        ]
-
-        let searchItems = [
-            PickerItem(title: "Test1", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Test2", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Test3", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Search1", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Search2", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Search3", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Search4", subtitle: "Test", item: "Test"),
-        ]
-
-        let selectedItems = [
-            PickerItem(title: "Selected1", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Selected2", subtitle: "Test", item: "Test"),
-            PickerItem(title: "Selected3", subtitle: "Test", item: "Test"),
-        ]
-
-        pickerInput?.searchPickerItems = { searchText, _, callback in
-            guard let searchText = searchText else { return }
-            let searchItems = searchItems.filter { $0.title?.localizedLowercase.contains(searchText.localizedLowercase) ?? false }
-            callback(searchItems)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                callback(searchItems)
-            }
-        }
-        pickerInput?.fetchFilterArray = { $0.title == $1.title }
-        pickerInput?.mapSelectedArray = { $0.item }
-        pickerInput?.itemCount = 10
-        pickerInput?.fetchPickerItems = { callback in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                callback(items, pickerInput?.selectedItems as? [PickerItem] ?? [])
-            }
-        }
-
-        pickerInput?.value = selectedItems
-//        pickerInput?.initialItems = items
-        
         
         formView?.onSubmit = { jsonValue in
             print(jsonValue)
@@ -91,12 +73,40 @@ class ViewController: UIViewController {
             tagTextInput,
             largeTextInput,
             pickerInput,
+            pickerInput2,
+            pickerInput3,
+            pickerInput4,
             datePickerInput,
             switchInput,
             submitButton,
         ]
         
         
+    }
+    
+    func createPickerInput() -> FormPickerView? {
+        let pickerInput = FormInputView.create(name: "picker", type: .Picker) as? FormPickerView
+        pickerInput?.title = "Seçmek istediğini seç"
+        pickerInput?.searchPickerItems = { searchText, _, callback in
+            guard let searchText = searchText else { return }
+            let searchItems = self.searchItems.filter { $0.title?.localizedLowercase.contains(searchText.localizedLowercase) ?? false }
+            callback(searchItems)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                callback(searchItems)
+            }
+        }
+        pickerInput?.fetchFilterArray = { $0.title == $1.title }
+        pickerInput?.mapSelectedArray = { $0.item }
+        pickerInput?.itemCount = 10
+        pickerInput?.fetchPickerItems = { callback in
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                callback(self.items, pickerInput?.selectedItems as? [PickerItem] ?? [])
+            }
+        }
+        
+        pickerInput?.backgroundColor = .blue
+        pickerInput?.value = selectedItems
+        return pickerInput
     }
     
 }
